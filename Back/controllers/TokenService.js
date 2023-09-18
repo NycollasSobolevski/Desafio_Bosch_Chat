57ac7b7ef222b2b566c47f28e343a764efe30034
LeonardoFalango <leo_falango@hotmail.com>
2023-09-18 09:51:21 -0300

    add: tokenService

---
+++ Back/controllers/TokenService.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+
+
+class TokenService {
+    static async generate(data) {
+        let secret = process.env.SECRET
+
+        let token = jwt.sign(
+            {
+                id: data._id
+            },
+            secret,
+            {
+                expiresIn: '2 days'
+            }
+        )
+
+        return token
+    }
+
+    static async verifyToken() {
+        const token = sessionStorage.getItem('token')
+        return jwt.verify(token, process.env.SECRET)
+    }
+}
+
+
+module.exports = TokenService
