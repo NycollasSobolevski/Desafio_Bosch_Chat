c790519944b9a49a720d3cdae28eddcf267cbff4
LeonardoFalango <leo_falango@hotmail.com>
2023-09-20 08:29:03 -0300

    add: ()

--- Back/controllers/PostController.js
+++ Back/controllers/PostController.js
@@ -7,25 +7,25 @@ const Comment = require('../model/Comment')
 
 class PostController {
     static async getAll(req, res) {
-        const { verbose, token } = req.body
-        const { page } = req.params
-
-        var isLogged = await TokenService.verifyToken(token)
-        if (!isLogged)
-            return Responses.Unauthorized(req, res)
-
-
-        if (verbose) {
-            console.log('forum getall')
-            console.log('Your token: ')
-            console.log(token)
-        }
-
         try {
+            const { verbose, token } = req.body
+            const { page } = req.params
+
+            var isLogged = await TokenService.verifyToken(token)
+            if (!isLogged)
+                return Responses.Unauthorized(req, res)
+
+
+            if (verbose) {
+                console.log('forum getall')
+                console.log('Your token: ')
+                console.log(token)
+            }
+
             const data = await Post
                 .find()
-                .sort( { postedAt : -1 })
-                .skip(20 * page - 1)
+                .sort( { postedAt : -1 } )
+                .skip(20 * (page - 1))
                 .limit(20 * page)
 
             return res.status(200).send({
